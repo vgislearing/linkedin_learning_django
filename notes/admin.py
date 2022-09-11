60bc69a0d85aebc93d2aff5f3c600c9ab9caa401
virgilio <aquinovirgilio@hotmail.com>
2022-09-11 21:38:46 +1000

    Baseline Files from Django course

---
+++ notes/admin.py
@@ -0,0 +1,10 @@
+from pydoc import ModuleScanner
+from django.contrib import admin
+
+from . import models
+
+# Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+admin.site.register(models.Notes, NotesAdmin)
