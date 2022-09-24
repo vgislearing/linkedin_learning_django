e32f84d85eac9488f691f118e496cc81c4b2a4e5
virgilio <vg@email.com>
2022-09-24 18:18:13 +1000

    4. A bit more on class-based views

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
-    path('notes/<int:pk>', views.detail),
+    path('notes', views.NotesListViews.as_view()),
+    path('notes/<int:pk>', views.NotesDetailsView.as_view()),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListViews(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+class NotesDetailsView(DetailView):
+    model = Notes
+    context_object_name = "note"
 
 def detail(request, pk):
     try:
